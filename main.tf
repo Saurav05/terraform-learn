6ceaf983d6b8d1bd2c8b6fc3878ad0020fd7f941
Saurav05 <sauravkhatri05@gmail.com>
2023-09-09 19:47:14 +0000

    docker added

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
+provider "docker" {
+}
+
+
 resource "local_file" "batch4file" {
-	filename = "/home/ubuntu/terraform-learn/demo.txt"
-	content =  "This is a file created with TeeraForm"
+  filename = "/home/ubuntu/terraform-learn/demo.txt"
+  content  = "This is a file created with TeeraForm"
+}
+
+resource "docker_image" "nginx" {
+	name = "nginx:latest"
+	keep_locally = false
+}
+
+resource "docker_container" "nginx-ctr" {
+	name = "my-nginx-container"
+	image = docker_image.nginx.name
+
+	ports {
+	internal = 80
+	external = 80
+	}
 }
